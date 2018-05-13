fe8d2f4c90aeefc0063a6283b4b00d3de0a6f791
Juliano Americo <juriam_man@hotmail.com>
2018-05-13 00:49:42 -0300

    Update the validation form to login

--- web/view/JS/script_js.js
+++ web/view/JS/script_js.js
@@ -1,9 +1,10 @@
 // Author: Juliano Américo
 // Description: JS for validation forms
 
+// Login validation
 function validateForm(){
-	var x = document.forms["loginForm"].value;
-	var y = document.forms["loginForm"].value;
+	var x = document.forms["loginForm"]["login"].value;
+	var y = document.forms["loginForm"]["password"].value;
 	if(x == "" || y == ""){
 		alert("Os campos devem ser preenchidos");
 	}
