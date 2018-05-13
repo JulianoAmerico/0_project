f7374e15042223b3673318ce88ef825b87b57810
Juliano Americo <juriam_man@hotmail.com>
2018-05-13 15:38:09 -0300

    Added the script with mask phone

--- web/view/JS/script_js.js
+++ web/view/JS/script_js.js
@@ -9,3 +9,23 @@ function validateForm(){
 		alert("Os campos devem ser preenchidos");
 	}
 }
+
+// Mask for forms
+function mask(o, f){
+	v_obj = o;
+	v_fun = f;
+    setTimeout("execmask()", 1);
+}
+
+function execmask(){
+	v_obj.value=v_fun(v_obj.value);
+}
+
+// Pattern phone (11) 1111-1111
+function phone(v){
+	
+	v=v.replace(/\D/g, ""); //Remove all the caracteres
+	v=v.replace(/^(\d\d)(\d)/g, "($1) $2"); //Put the '()' between the first numbers
+    v=v.replace(/(\d{4})(\d)/, "$1-$2"); //Put the '-' between fourth number and fifth number.
+    return v;
+}
